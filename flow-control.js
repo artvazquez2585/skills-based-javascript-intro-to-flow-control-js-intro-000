6e330c3793c0151b684cc226f2223e15f61ac07a
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-29 06:40:13 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,5 +27,10 @@ return (age>=13 && age<=19) ? 'You are a teenager' : 'You are not a teenager'
 }
 
 function switchAge(age) {
-
+switch(age<=13 && age>=19) {
+  case true:
+  return 'You are a teenager'
+  case false:
+  return 'You have an age'
+}
 }
